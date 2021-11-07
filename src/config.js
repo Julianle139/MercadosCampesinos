a818c511cb6fd35d4a392e36a0d29c3e7de51efd
Julian Gallo <julianleonardogallo@gmail.com>
2021-11-07 15:20:59 -0500

    Config V1

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 //Ruta conexion con base de datos
 export let Path = {
-  url: "https://mercadoscampesinos.herokuapp.com/api",
+  url: "mongodb+srv://Admin:admin@cluster0.t6b1o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
