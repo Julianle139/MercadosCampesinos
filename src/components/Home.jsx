be7e417efa8ff285e17db36163a214b5bef5936d
Julian Gallo <julianleonardogallo@gmail.com>
2021-11-07 00:06:14 -0500

    despliegue con heroku

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,8 +21,8 @@ export default function Home() {
          <LoginButton/>
         <div className="cont">
             <div className="cont-home">
-                <h1>Sistema Gestión de la Papelería San Vicente.</h1>
-                <h2>Ubicados en el barrio 7 de Agosto en Bogotá, Colombia. Con más de 50 años de expericiencia en la comercialización de productos escolares, de oficina y obsequios</h2>
+                <h1>Sistema Gestión de Mercado Campesino.</h1>
+                <h2>Prueba</h2>
                 <p>Aquí puede realizar tareas de gestión como registrar, actualizar y consultar ventas. Consultar nuestro portafolio de productos</p>
             </div>
             <Contact/>
